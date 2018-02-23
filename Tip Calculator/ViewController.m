aab010cf201db865582ffe3ab4d19e02f255c989
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-23 14:08:59 -0800

    added text fields

--- Tip Calculator/ViewController.m
+++ Tip Calculator/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (nonatomic) BOOL didEnterTipPercentage;
 
 @end
 
@@ -16,13 +21,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //[self.tipPercentageTextField setDelegate:self];
+    self.tipPercentageTextField.delegate = self;
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    [self.view addGestureRecognizer:tap];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)textField {
+    
+    self.didEnterTipPercentage = YES;
+}
+
+-(void)dismissKeyboard {
+    [self.billAmountTextField resignFirstResponder];
+    [self.tipPercentageTextField resignFirstResponder];
+}
+
+- (IBAction)calculateTip:(id)sender {
+    float tipPercentage;
+    
+    if(self.didEnterTipPercentage) {
+        tipPercentage = [self.tipPercentageTextField.text floatValue]/100;
+    } else {
+        tipPercentage = 0.15;
+    }
+    
+    NSString *billAmount = self.billAmountTextField.text;
+    float billFloatAmount = [billAmount floatValue];
+    float tipAmount = billFloatAmount*tipPercentage;
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"$%.02f", tipAmount];
+    
+    self.didEnterTipPercentage = NO;
+    self.tipPercentageTextField.text = @"";
 }
 
 
