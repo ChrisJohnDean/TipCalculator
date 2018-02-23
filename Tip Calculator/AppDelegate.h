f2680a925d905ff149a88c811c29fdc5ce7deca1
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-23 12:47:49 -0800

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Chris Dean on 2018-02-23.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Chris Dean on 2018-02-23.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
