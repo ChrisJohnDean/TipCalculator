f6d4c78cf5cee4214897c23d7e469be2f9c82033
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-23 16:12:51 -0800

    added slider functionality for tip percentage

--- Tip Calculator/ViewController.m
+++ Tip Calculator/ViewController.m
@@ -29,6 +29,17 @@
     [self.view addGestureRecognizer:tap];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -36,8 +47,21 @@
 }
 
 -(void)textFieldDidBeginEditing:(UITextField *)textField {
-    
     self.didEnterTipPercentage = YES;
+
+}
+
+-(void)keyboardDidShow:(NSNotification*)notification {
+    
+    [UIView animateWithDuration:2 animations:^{
+        [self.view setFrame:CGRectOffset(self.view.frame, 0, -100)];
+    }];
+}
+
+-(void)keyboardDidHide:(NSNotification*)notification {
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view setFrame:CGRectOffset(self.view.frame, 0, 100)];
+    }];
 }
 
 -(void)dismissKeyboard {
@@ -61,6 +85,15 @@
     
     self.didEnterTipPercentage = NO;
     self.tipPercentageTextField.text = @"";
+}
+
+- (IBAction)adjustTipPercentage:(UISlider*)sender {
+    float tipPercentage = sender.value/100;
+    self.tipPercentageTextField.text = [NSString stringWithFormat:@" %.1f", [sender value]];
+    NSString *billAmount = self.billAmountTextField.text;
+    float billFloatAmount = [billAmount floatValue];
+    float tipAmount = billFloatAmount*tipPercentage;
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"$%.02f", tipAmount];
 }
 
 
